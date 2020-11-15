7ab29268a286fd3c0c0a2cd778b46bc84a0e54da
HUANCHENZH <55564232+HUANCHENZH@users.noreply.github.com>
2020-11-16 00:23:45 +0800

    update

---
+++ soundboard.js
@@ -0,0 +1,17 @@
+ var getarrow = document.getElementById("arrowsound");
+
+ var getbomb = document.getElementById("bombsound");
+
+ var getdis = document.getElementById("dissound");
+
+function arrow() {
+    getarrow.play();
+}
+
+function bomb() {
+   getbomb.play();
+}
+
+function dis() {
+    getdis.play();
+}
